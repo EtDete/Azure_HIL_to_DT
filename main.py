53bb020edb6a462513ceadea4f0afd182d1cb39b
Etienne Delate <etienne.delate5@orange.fr>
2023-04-20 17:01:02 +0200

    20/04/2023

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import modbus
 import Iot_message_mqtt
 import parameter as param
+from time import sleep
 
-#data = modbus.run_modbu_task(param.ip_addr,param.port,param.request_addr)
+#f = open("Valeurs.txt","+a")
+
 path_to_root_cert = "<local path to digicert.cer file>"
 device_id = "Wind_unit"
 sas_token = "jw92w3o43TpsXpNGEmb1bXAsRrWvtGSedWGE5zWHYGQ="
@@ -10,3 +12,16 @@ iot_hub_name = "MicrogridHub"
 data = 24
 Iot_message_mqtt.run_client(device_id,path_to_root_cert,sas_token,iot_hub_name,data)
 
+# client = modbus.modbus_connexion(param.ip_addr,param.port)
+# data_0 = 10  #Variable representing the previous data received
+# while True :
+#     try:
+#         data = modbus.run_modbu_task(param.request_addr)
+#         f.write(str(data))
+#         if data != data_0 :
+#             data_0 = data
+#             Iot_message_mqtt.run_client(device_id,path_to_root_cert,sas_token,iot_hub_name,data)
+#         sleep(2)
+        
+#     except Exception:
+#         client.close
--- modbus.py
+++ modbus.py
@@ -2,35 +2,13 @@
 
 from pymodbus.client.tcp import ModbusTcpClient
 from time import sleep
-import socket
 import parameter as param
+
 def setup_client(ip_addr,port):
     #create an instance of modbus server
     client = ModbusTcpClient(ip_addr,port)
     return(client)
 
-def setup_socket():
-    """Permet de setup la connexion entre le HIL et le PC avec un socket
-    """
-    try:
-        print("Creating the socket")
-        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        print("Bind to the port")
-        s.bind((param.ip_addr,param.port))
-        print("Server created")
-        s.listen(1)
-        conn, addr = s.accept()
-        print("connection accepted")
-    except socket.timeout as e:
-        print(e)
-        print("Timeout: unable to bind to IP:"+param.ip_addr+'port:'+param.port+".\n")
-        return
-    except socket.error as e:
-        (errno,msg_str)= e.args
-        print(msg_str)
-        print("Timeout: unable to bind to IP:"+param.ip_addr+'port:'+param.port+".\n")
-        return
        
 def setup_connection_client(client):
     """Opening the connection between the client and the server
@@ -46,25 +24,26 @@ def setup_connection_client(client):
         print("Error")
         client.close()
         print("client disconnected")
+        
+def modbus_connexion(ip_addr,port):
+    client = setup_client(ip_addr,port)
+    setup_connection_client(client)
+    return(client)
 
-def run_modbu_task(ip_addr,port,request_addr):
-    """This function setup a modbus connection between a master and a client (device) and read a holding register at the address 40122
+def run_modbu_task(client,request_addr):
+    """This function setup a modbus connection between a master and a client (device) and read a holding register at a given address
 
     Args:
         ip_addr (str): ipv4 address of the server
         port (int): port number of the server, usually 502
     """
-    client = setup_client(ip_addr,port)
-    #setup_socket()
-    setup_connection_client(client)
     print("Starting the request")
     #count= the number of registers to read
     #unit= the slave unit this request is targeting
     #address= the starting address to read from
     read = client.read_holding_registers(address=request_addr,count=1)
-    print(read)
-    data=read.registers[0] #reading the registers 30122
+    data = read.registers[0] #reading the registers 30122
     print(data)
-    client.close()
+    #client.close()
     #terminate the connection
     return data
