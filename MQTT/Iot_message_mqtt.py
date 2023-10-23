ff59079b7552ea63048533fbacb427f27e34d63a
Etienne Delate <etienne.delate5@orange.fr>
2023-10-23 14:18:09 +0200

    23/10/2023

--- MQTT/Iot_message_mqtt.py
+++ MQTT/Iot_message_mqtt.py
@@ -2,7 +2,7 @@ from paho.mqtt import client as mqtt
 import ssl
 import struct
 
-path_to_root_cert = "C:\\Users\\etien\\OneDrive\\Bureau\\LIS\\Azure_HIL_to_DT\\certificate.txt"
+#path_to_root_cert = "C:\\Users\\etien\\OneDrive\\Bureau\\LIS\\Azure_HIL_to_DT\\certificate.txt"
 device_id = "Wind_unit"
 sas_token = "jw92w3o43TpsXpNGEmb1bXAsRrWvtGSedWGE5zWHYGQ="
 iot_hub_name = "MicrogridHub"
@@ -24,7 +24,7 @@ def create_client(device_id):
     client = mqtt.Client(client_id=device_id, protocol=mqtt.MQTTv311, transport="tcp")
     return(client)
 
-def connect_iot_hub(device_id, client,path_to_root_cert,sas_token,iot_hub_name):
+def connect_iot_hub(device_id, client,sas_token,iot_hub_name):
     client.on_connect = on_connect
     client.on_disconnect = on_disconnect
     client.on_publish = on_publish
@@ -32,7 +32,7 @@ def connect_iot_hub(device_id, client,path_to_root_cert,sas_token,iot_hub_name):
     client.username_pw_set(username=iot_hub_name+".azure-devices.net/" +
                        device_id + "/?api-version=2021-04-12", password=sas_token)
 
-    client.tls_set(ca_certs=path_to_root_cert, certfile=None, keyfile=None,
+    client.tls_set(certfile=None, keyfile=None,
                cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)
     client.tls_insecure_set(False)
 
